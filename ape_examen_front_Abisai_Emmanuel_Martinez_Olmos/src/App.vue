5dffcd7590c33bca0e5124d8240f0738da7f8473
Emmanuel Martinez <82051883+ShaviAEMO@users.noreply.github.com>
2023-07-27 19:43:01 -0600

    component

--- ape_examen_front_Abisai_Emmanuel_Martinez_Olmos/src/App.vue
+++ ape_examen_front_Abisai_Emmanuel_Martinez_Olmos/src/App.vue
@@ -1,47 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <tabla_usuarios :usuarios="usuarios" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script>
+import TablaUsuarios from './components/tabla_usuarios/Tabla_usuarios.vue';
+import axios from 'axios';
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  components: {
+    tabla_usuarios: TablaUsuarios
+  },
+  data() {
+    return {
+      usuarios: []
+    };
+  },
+  created() {
+    this.obtenerUsuarios();
+  },
+  methods: {
+    obtenerUsuarios() {
+      axios.get('http://localhost:8000/api/users')
+          .then(response => {
+            this.usuarios = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    }
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
